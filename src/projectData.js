409aa68c2e693932231df367ef8f9694d4034fd8
Steven Klinefelter <134639644+Klinefelters@users.noreply.github.com>
2023-12-19 01:10:31 -0500

    Add Solana Word Wave project and update OmniPi description

--- src/projectData.js
+++ src/projectData.js
@@ -11,18 +11,18 @@ export const projects = [
     //     buttons: [{ name: 'GitHub', url: 'https://github.com/Klinefelters/SignalNoise' }],
     //     video: '/OmniPi.mp4'
     // },
-    // {
-    //     title: 'Solana Word Wave',
-    //     description: 'This is project 1',
-    //     buttons: [
-    //         { name: 'GitHub', url: 'https://github.com/Etown-Computer-Science-Club/SolanaWordWave' },
-    //         { name: 'Website', url: 'https://wordwave.us' }
-    //     ],
-    //     video: '/OmniPi.mp4'
-    // },
+    {
+        title: 'Solana Word Wave',
+        description: 'A daily word vocabulary expansion game that rewards players with tokens on the Solana blockchain.',
+        buttons: [
+            { name: 'GitHub', url: 'https://github.com/Etown-Computer-Science-Club/SolanaWordWave' },
+            { name: 'Website', url: 'https://wordwave.us' }
+        ],
+        video: '/wordwave.mp4'
+    },
     {
         title: 'OmniPi',
-        description: 'Using a Raspberry Pi 3b, n20 motors, L298N motor controllers, and mecanum wheels, I created a robot that can move in any direction. Making use of a custom designed web interface, I was able to control the robot from any device on the hotspot hosted by the Pi. ',
+        description: 'Using a Raspberry Pi 3b, I created a robot that can move in any direction using a custom programmed web interface. ',
         buttons: [{ name: 'GitHub', url: 'https://github.com/Klinefelters/OmniPi' }],
         video: '/OmniPi.mp4'
     },
